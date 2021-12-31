c5bf74144ef67a46a6f4c85312f5e15cb8b0b190
LaaGostim <LaaGostim.OFICIAL@gmail.com>
2021-12-31 14:57:37 +0000

    main commit

---
+++ 4-menu/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+//array "allCategories", começa com "all", depois é feito o spread(...) para adicionar o resto das categorias, o "new Set" é necessário para não repetir as categorias
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ 6-cocktails/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+        repudiandae porro numquam beatae natus maxime veritatis suscipit vitae
+        provident alias! Placeat recusandae similique accusamus dolorum,
+        voluptatibus, aut quae explicabo eum quibusdam labore velit! Earum atque
+        officiis aliquid eligendi, voluptatibus eos!
+      </p>
+    </section>
+  );
+};
+
+export default About;
